7321ec7145f49e3c42165349a9d56fd1be7dd01b
Arror <hallo.maqiang@icloud.com>
2023-09-12 16:04:35 +0800

    Remove web router

--- server/router.go
+++ server/router.go
@@ -7,7 +7,6 @@ import (
 	"github.com/alist-org/alist/v3/server/common"
 	"github.com/alist-org/alist/v3/server/handles"
 	"github.com/alist-org/alist/v3/server/middlewares"
-	"github.com/alist-org/alist/v3/server/static"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -68,9 +67,6 @@ func Init(e *gin.Engine) {
 	if flags.Debug || flags.Dev {
 		debug(g.Group("/debug"))
 	}
-	static.Static(g, func(handlers ...gin.HandlerFunc) {
-		e.NoRoute(handlers...)
-	})
 }
 
 func admin(g *gin.RouterGroup) {
